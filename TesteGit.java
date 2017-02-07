b61a4da1d62902ad9847ce99dde0d54abd3331af
gugaucb <gugaucb@gmail.com>
2017-02-07 15:06:07 -0200

    mais uma mensagem

--- TesteGit.java
+++ TesteGit.java
@@ -3,5 +3,6 @@ public class TesteGit{
 	public static void main(String[] args){
 		//comentario
 		System.out.println("Exemplo de teste de uso do git");
+		System.out.println("Trabalhando com branch");
 	}
 }
